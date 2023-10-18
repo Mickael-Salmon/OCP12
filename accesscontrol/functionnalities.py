04c46654dc15366972d04895f6e0c1d684281e81
Mickael Salmon <mickael.salmon@outlook.com>
2023-10-18 17:23:30 +0200

    Added JSON data content files to insert later in the database

---
+++ accesscontrol/functionnalities.py
@@ -0,0 +1,64 @@
+﻿"""
+This file is central to the authentication process in the application.
+It provides functionalities for logging in, signing up, and logging out users.
+It validates user credentials, creates JWT tokens for authenticated sessions,
+and manages the storing and clearing of these tokens.
+"""
+from sqlalchemy.orm import Session
+import sqlalchemy
+
+from models.employees import Employee, Department
+from database.manager import engine
+from authentification.token import create_token, store_token, clear_token
+
+
+def login(email: str, password: str) -> Employee:
+    """
+    Create a JWT containing the user ID and store it on the user's disk.
+    The application will later check this stored token to determine whether the user is authenticated.
+    Returns the retrieved Employee object if the login was successful, otherwise returns None.
+    """
+    session = Session(engine)
+    request = sqlalchemy.select(Employee).where(Employee.email == email)
+
+    employee = session.scalar(request)
+
+    if not employee:
+        return None
+
+    password_is_valid = employee.check_password(password)
+
+    if password_is_valid:
+        token = create_token(user_id=employee.id)
+        store_token(token)
+        return employee
+    else:
+        clear_token()
+        return None
+
+
+def sign_up(full_name: str, email: str, password: str):
+    """
+    Create a new user in the database without being logged in, then log in the created user.
+    As only accounting employees are allowed to create users, the new user will be assigned to the accounting department.
+    """
+    with Session(engine) as session:
+        new_employee = Employee(
+            full_name=full_name,
+            email=email,
+            department=Department.ACCOUNTING,
+        )
+
+        new_employee.set_password(password)
+
+        session.add(new_employee)
+        session.commit()
+
+    login(email, password)
+
+
+def logout():
+    """
+    Clear the JWT stored on the user's disk, effectively logging out the user.
+    """
+    clear_token()
---
+++ accesscontrol/sec_sessions.py
@@ -0,0 +1,70 @@
+﻿"""
+This file contains decorators used to address security mechanisms within the application like authorization and authentication.
+It ensures that only authenticated users can access certain parts of the application and that they have the appropriate permissions.
+It contains decorators that are used to protect routes and functions, ensuring that only authorized and authenticated users can perform certain actions.
+It does maintain the integrity and confidentiality of the application data.
+"""
+
+from sqlalchemy.orm import Session
+import sqlalchemy
+import typing
+
+from authentification.token import decode_token, retreive_token
+from models.employees import Employee, Department
+from database.manager import engine
+
+def login_required(function):
+    """
+    Decorator for checking if the current user is authenticated.
+    It tries to retrieve and decode the token stored on the user's disk.
+
+    Raises:
+    * PermissionError: If authentication fails.
+    """
+    def wrapper(*args, **kwargs):
+        token = retreive_token()
+
+        if not decode_token(token):
+            raise PermissionError("Please login and retry.")
+
+        return function(*args, **kwargs)
+
+    return wrapper
+
+def permission_required(roles: typing.List[Department]):
+    """
+    Decorator for checking if the authenticated user belongs to a certain department.
+
+    Args:
+    * roles (List[Department]): A list of Department enums that are authorized to access the function.
+
+    Raises:
+    * PermissionError: If the user does not have permission to access the function.
+    """
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            REJECT_MESSAGE = "Permission denied."
+
+            token = retreive_token()
+            token_payload = decode_token(token)
+
+            if not token_payload:
+                raise PermissionError(REJECT_MESSAGE)
+
+            user_id = token_payload["user_id"]
+
+            session = Session(engine)
+            request = sqlalchemy.select(Employee).where(Employee.id == user_id)
+            employee = session.scalar(request)
+
+            if not employee:
+                raise PermissionError(REJECT_MESSAGE)
+
+            if employee.department not in roles:
+                raise PermissionError(REJECT_MESSAGE)
+
+            return function(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
